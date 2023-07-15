50bcf68e632833b9fd6653d08cfebe2550f2cc75
Abu Sayed <mdsayed81@gmail.com>
2023-07-15 17:00:02 +0600

    update

--- function5/index.js
+++ function5/index.js
@@ -1,21 +1,25 @@
-// Funtion Declearation
-
-//   function greetings(){
-//   	console.log('Hello World');
-//   	document.write('hello there' + "<br>");
-// }
-// greetings();
-// greetings();
-// greetings();
-
-
-//Function Expression
-
-const speak = function(fName,lName){
-	console.log(`${fName} ${lName}`);
-  	document.write(`${fName} ${lName} <br>`);
+class Student{
+	constructor(name,age,cls){
+		this.myname = name;
+		this.myage = age;
+		this.mycls = cls;
+	}
+	student_profile(){
+		document.write(`My name is ${this.myname}. My age is ${this.myage}. My class is ${this.mycls}`);
+		return`My name is ${this.myname}. My age is ${this.myage}. My class is ${this.mycls}`;
+	}
 }
 
-speak('Abu','Sayed');
-speak('Hasan','Ali');
-speak('Raihanul','Islam');
+//Inharitance
+
+class Player extends Student{
+	constructor(name,age,cls,game){
+		super(name,age,cls);
+		this.mygame = game;
+	}
+	player_profile(){
+		return`${this.student_profile()}. I love to play ${this.mygame}`
+	}
+}
+const student1 = new Player('Abu Sayed', '40', 'B.com', 'Football');
+console.log(student1.player_profile());
