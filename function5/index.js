34097d9fde7b60ac9276ad8311a95ce26884d659
Abu Sayed <mdsayed81@gmail.com>
2023-07-15 07:53:39 +0600

    JavaScript Class, Inheritance

---
+++ function5/index.js
@@ -0,0 +1,21 @@
+// Funtion Declearation
+
+//   function greetings(){
+//   	console.log('Hello World');
+//   	document.write('hello there' + "<br>");
+// }
+// greetings();
+// greetings();
+// greetings();
+
+
+//Function Expression
+
+const speak = function(fName,lName){
+	console.log(`${fName} ${lName}`);
+  	document.write(`${fName} ${lName} <br>`);
+}
+
+speak('Abu','Sayed');
+speak('Hasan','Ali');
+speak('Raihanul','Islam');
