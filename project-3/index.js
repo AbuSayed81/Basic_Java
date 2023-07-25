aa274be002ea30036f66ecfdce5c6e95eb25adf7
Abu Sayed <mdsayed81@gmail.com>
2023-07-25 17:29:16 +0600

    1

--- project-3/index.js
+++ project-3/index.js
@@ -1,11 +1,6 @@
-const text = document.querySelector(".text");
-const read_more_btn = document.querySelector(".read_more_btn");
 
-read_more_btn.addEventListener('click', (e)=>{
-    text.classList.toggle('show-more');
-    if(read_more_btn.innerHTML === 'Read More'){
-        read_more_btn.innerHTML = 'Read Less';
-    }else{
-        read_more_btn.innerHTML = 'Read More';
-    }
+const header = document.querySelector("header");
+
+window.addEventListener('scroll', ()=>{
+    header.classList.toggle('sticky', window.scrollY > 0);
 })
